09c6812a564409105adb5b19f27a3d5dba5fb7fd
Duran Thomas <22084776+duran-thomas@users.noreply.github.com>
2025-03-30 20:44:10 -0500

    test page

---
+++ src/app/page-home.js
@@ -0,0 +1,12 @@
+import IndexMain from "@/components/layout/main/IndexMain";
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+
+export default function Home() {
+  return (
+    <PageWrapper>
+      <IndexMain />
+    </PageWrapper>
+  );
+}
+
+
